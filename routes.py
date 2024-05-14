06279ec1b2d574a8e5f36e291a86a66606d62bb9
Medenre <mederic56800@gmail.com>
2024-05-14 22:40:31 +0200

    commit

--- routes.py
+++ routes.py
@@ -18,6 +18,7 @@ def init_app(app):  #POUR INIT APP.PY
            flash('Veuillez vous connecter pour accéder à cette fonctionnalité.', 'warning')
            return redirect(url_for('index'))
         current_user = session.get('username')
+
         # Récupérer les données de la base de données
         open_tickets = Ticket.query.filter_by(status='Ouvert').count()
         closed_tickets = Ticket.query.filter_by(status='Fermer').count()
@@ -26,7 +27,7 @@ def init_app(app):  #POUR INIT APP.PY
 
         return render_template('index.html' , current_user=current_user,open_tickets=open_tickets, closed_tickets=closed_tickets, in_progress_tickets=in_progress_tickets, resolved_tickets=resolved_tickets)
     
-    #PAGE TICKET
+    # PAGE TICKET
     @app.route('/ticket')
     def ticket():
         if 'user_id' not in session:
@@ -36,7 +37,32 @@ def init_app(app):  #POUR INIT APP.PY
         current_user = session.get('username')
         return render_template('ticket.html', tickets=tickets, current_user=current_user)
     
-    #FONCTION LOGIN
+    #PAGE DASHBOARD
+    @app.route('/emb_deb')
+    def emb_deb():
+        if 'user_id' not in session:
+           flash('Veuillez vous connecter pour accéder à cette fonctionnalité.', 'warning')
+           return redirect(url_for('index'))
+        current_user = session.get('username')
+
+        return render_template('emb_deb.html' , current_user=current_user)
+    
+
+    # PAGE MATERIEL
+    @app.route('/materiel')
+    def materiel():
+        if 'user_id' not in session:
+           flash('Veuillez vous connecter pour accéder à cette fonctionnalité.', 'warning')
+           return redirect(url_for('index'))
+        current_user = session.get('username')
+        form = MaterielForm()
+        materiels = Materiel.query.all()
+        locations = Location.query.all()
+
+        update_success = session.pop('update_success', False)
+        return render_template('materiel.html', form=form ,current_user=current_user, materiels=materiels, locations=locations, update_success=update_success)
+
+    # FONCTION LOGIN
     @app.route('/login', methods=['GET', 'POST'])
     def login():
         if request.method == 'POST':
@@ -53,27 +79,13 @@ def init_app(app):  #POUR INIT APP.PY
                 error = "Identifiant ou mot de passe incorrect."
         return render_template('login.html', error=error)
     
-    @app.route('/materiel')
-    def materiel():
-        if 'user_id' not in session:
-           flash('Veuillez vous connecter pour accéder à cette fonctionnalité.', 'warning')
-           return redirect(url_for('index'))
-        current_user = session.get('username')
-        form = MaterielForm()
-        materiels = Materiel.query.all()
-        locations = Location.query.all()
-
-        update_success = session.pop('update_success', False)
-        return render_template('materiel.html', form=form ,current_user=current_user, materiels=materiels, locations=locations, update_success=update_success)
-
-
+    
     @app.route('/admin/dashboard')  
     def admin_dashboard():
         if 'user_id' not in session or not session['is_admin']:
             abort(403)  # Accès interdit$
         else:
-            return render_template('index.html')
-        
+            return render_template('index.html')   
 
     @app.route('/create_ticket', methods=['GET', 'POST'])
     def create_ticket():
@@ -167,7 +179,6 @@ def init_app(app):  #POUR INIT APP.PY
         
         return redirect(url_for('materiel'))
          
-    
     @app.route('/respond_ticket/<int:ticket_id>', methods=['GET', 'POST'])
     def respond_ticket(ticket_id):
         ticket = Ticket.query.get_or_404(ticket_id)
@@ -180,7 +191,6 @@ def init_app(app):  #POUR INIT APP.PY
             return redirect(url_for('index'))
         return render_template('respond_ticket.html', ticket=ticket)
 
-   
     @app.route('/diagramme')
     def diagramme():
         # Transmettre les données au modèle HTML
