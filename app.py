8cd2dcc17315b21277f0dacea46a9e520e0cc432
Medenre <108235842+Medenre@users.noreply.github.com>
2024-08-27 10:31:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SECRET_KEY'] = b'\x97\xfd\xb8\x98j-\xd1;$\xbb\xe2Z\xc6\x0c\x87t' #python -c 'import os; print(os.urandom(16))'
 
+@app.after_request
+def add_header(response):
+    response.headers['Content-Security-Policy'] = "default-src 'self'; script-src 'self' https://kit.fontawesome.com; style-src 'self' https://kit.fontawesome.com;"
+    return response
+
 # Initialise la base de données
 db.init_app(app)
 
